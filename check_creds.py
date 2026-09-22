a3e09def1ca9894924d82d96bc520e8c5b2a6e5a
vipman23-25 <vipman23@gmail.com>
2026-09-22 16:19:48 +0300

    Otomatik güncelleme: Vercel, Neon ve Github

--- check_creds.py
+++ check_creds.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect('instance/cms.db')
-c = conn.cursor()
-c.execute("SELECT key, value FROM setting")
-rows = c.fetchall()
-print('All settings:', rows)
-conn.close()
+import psycopg2
+conn = psycopg2.connect('postgresql://neondb_owner:npg_PHdtr61ILkWz@ep-restless-feather-b25l03vt-pooler.c-6.eu-central-1.aws.neon.tech/neondb?sslmode=require')
+cur = conn.cursor()
+cur.execute("SELECT key, value FROM setting")
+print('Settings in Neon:', cur.fetchall())
