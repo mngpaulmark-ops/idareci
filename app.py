f2a9c9e6774630c355ca4ca3a8f3813c484e20eb
vipman23-25 <vipman23@gmail.com>
2026-09-22 19:47:15 +0300

    Add gallery edit and delete functionality

--- app.py
+++ app.py
@@ -3559,3 +3559,24 @@ def admin_backup():
                     pass
                     
     return send_file(zip_path, as_attachment=True, download_name='site_tam_yedek.zip')
+
+@app.route('/admin/galeri/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def admin_galeri_edit(id):
+    g = Galeri.query.get_or_404(id)
+    if request.method == 'POST':
+        g.title = request.form.get('title')
+        g.date = request.form.get('date')
+        db.session.commit()
+        return redirect(url_for('admin_galeri'))
+    return render_template('admin/galeri_edit.html', galeri=g)
+
+@app.route('/admin/galeri/sil/<int:id>', methods=['POST'])
+@login_required
+def admin_galeri_sil(id):
+    g = Galeri.query.get_or_404(id)
+    for resim in g.resimler:
+        db.session.delete(resim)
+    db.session.delete(g)
+    db.session.commit()
+    return redirect(url_for('admin_galeri'))
