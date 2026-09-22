9794a2f896e5b9c94d55f3a5fbfcffd797d6db47
vipman23-25 <vipman23@gmail.com>
2026-09-22 19:44:44 +0300

    Add global read-only file system patches for Vercel

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
 from werkzeug.utils import secure_filename
 
 import os
+import builtins
+import os
+
+# 1. Patch open() to prevent Read-Only crashes on Vercel
+original_open = builtins.open
+def safe_open(file, mode='r', *args, **kwargs):
+    if isinstance(mode, str) and ('w' in mode or 'a' in mode or '+' in mode):
+        try:
+            return original_open(file, mode, *args, **kwargs)
+        except OSError:
+            import io
+            if 'b' in mode:
+                return io.BytesIO()
+            else:
+                return io.StringIO()
+    return original_open(file, mode, *args, **kwargs)
+builtins.open = safe_open
+
+# 2. Patch os.makedirs
+original_makedirs = os.makedirs
+def safe_makedirs(*args, **kwargs):
+    try:
+        original_makedirs(*args, **kwargs)
+    except OSError:
+        pass
+os.makedirs = safe_makedirs
+
 
 from werkzeug.datastructures import FileStorage
 def dummy_save(self, *args, **kwargs):
